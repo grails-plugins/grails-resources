16e0a48d75510311091663bf1dd1c9001bf4d956
ldaley <ldaley@832c1c66-4827-0410-8465-ccb17913cd09>
2010-10-15 04:03:12 +0000

    unused imports.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-resources/trunk@65369 832c1c66-4827-0410-8465-ccb17913cd09

--- src/java/org/grails/plugin/resources/artefacts/ResourceMapperClass.java
+++ src/java/org/grails/plugin/resources/artefacts/ResourceMapperClass.java
@@ -1,7 +1,5 @@
 package org.grails.plugin.resources.artefacts;
 
 import org.codehaus.groovy.grails.commons.GrailsClass;
-import org.grails.plugin.resource.ResourceMeta;
-import java.util.Comparator;
 
 public interface ResourceMapperClass extends GrailsClass {}
