d8ad987b686ada15968eb07657e24b4d19bacfcb
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2012-12-18 14:15:20 -0700

    Reformat long line.

--- grails-app/conf/TestOnlyResources.groovy
+++ grails-app/conf/TestOnlyResources.groovy
@@ -5,7 +5,9 @@ environments {
                 resource url:'js/jquery/jquery-1.4.2-b.min.js', disposition:'head'
             }
 			testurl {
-				resource url: 'http://fonts.googleapis.com/css?family=PT+Sans:400,700&subset=latin,cyrillic', disposition: 'head', attrs: [type: 'css']
+				resource url: 'http://fonts.googleapis.com/css?family=PT+Sans:400,700&subset=latin,cyrillic', 
+				          disposition: 'head', 
+						  attrs: [type: 'css']
 			}
         }
     }
