1e6043b2fc8532b583460c930d147b75ca6e4557
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2012-12-19 16:30:20 -0700

    Add test with wrapper in resource which fails.

--- grails-app/conf/TestOnlyResources.groovy
+++ grails-app/conf/TestOnlyResources.groovy
@@ -4,6 +4,12 @@ environments {
             'jquery' { 
                 resource url:'js/jquery/jquery-1.4.2-b.min.js', disposition:'head'
             }
+			
+			'testAppAccess' {
+				resource id:'testAppAccess', 
+				         url: "css/main.css",
+						 wrapper: {"<!--${grailsApplication.ENV_DEVELOPMENT}-->"}
+			}
         }
     }
 }
