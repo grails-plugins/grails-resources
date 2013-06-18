ab0fb152e944e37417c442bfacc7b042361bd378
Patrick Jungermann <patrick.jungermann@kaufda.de>
2013-06-18 03:01:43 +0200

    GPRESOURCES-214 - fix LegacyResourceIntegrationSpec (test mapper adds an underscore as prefix to all resources)

--- test/integration/org/grails/plugin/resource/LegacyResourceIntegrationSpec.groovy
+++ test/integration/org/grails/plugin/resource/LegacyResourceIntegrationSpec.groovy
@@ -1,29 +1,30 @@
 package org.grails.plugin.resource
 
 import grails.plugin.spock.IntegrationSpec
+import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletRequest
 import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletResponse
 
 class LegacyResourceIntegrationSpec extends IntegrationSpec {
     
-    def grailsResourceProcessor
-    def grailsApplication
+    ResourceProcessor grailsResourceProcessor
+    GrailsApplication grailsApplication
     
     // GPRESOURCES-214
     def 'legacy resource with baseurl'() {
-        
         grailsApplication.config.grails.resources.mappers.baseurl.enabled = true
         grailsApplication.config.grails.resources.mappers.baseurl.default = "http://cdn.domain.com/static"
+
+        GrailsMockHttpServletRequest request = new GrailsMockHttpServletRequest()
+        GrailsMockHttpServletResponse response = new GrailsMockHttpServletResponse()
         
-        def request = new GrailsMockHttpServletRequest()
-        def response = new GrailsMockHttpServletResponse()
-        
-        request.setRequestURI("/images/springsource.png")
+        request.requestURI = "/images/springsource.png"
         
         when:
             grailsResourceProcessor.processLegacyResource(request, response)
+
         then:
-            response.redirectedUrl == "http://cdn.domain.com/static/images/springsource.png"
+            response.redirectedUrl == "http://cdn.domain.com/static/images/_springsource.png"
     }
     
 }
