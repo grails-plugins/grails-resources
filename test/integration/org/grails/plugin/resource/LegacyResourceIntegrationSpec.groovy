31307354d3f0c48e0956a3fd72ce57a4183af75b
Patrick Jungermann <patrick.jungermann@kaufda.de>
2013-06-18 02:57:19 +0200

    Revert 'Revert "GPRESOURCES-214 - integration test"''
    This reverts commit 98ac01d11b112650fe7d1a804bcb0d050d3dc6b4.

---
+++ test/integration/org/grails/plugin/resource/LegacyResourceIntegrationSpec.groovy
@@ -0,0 +1,29 @@
+package org.grails.plugin.resource
+
+import grails.plugin.spock.IntegrationSpec
+import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletRequest
+import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletResponse
+
+class LegacyResourceIntegrationSpec extends IntegrationSpec {
+    
+    def grailsResourceProcessor
+    def grailsApplication
+    
+    // GPRESOURCES-214
+    def 'legacy resource with baseurl'() {
+        
+        grailsApplication.config.grails.resources.mappers.baseurl.enabled = true
+        grailsApplication.config.grails.resources.mappers.baseurl.default = "http://cdn.domain.com/static"
+        
+        def request = new GrailsMockHttpServletRequest()
+        def response = new GrailsMockHttpServletResponse()
+        
+        request.setRequestURI("/images/springsource.png")
+        
+        when:
+            grailsResourceProcessor.processLegacyResource(request, response)
+        then:
+            response.redirectedUrl == "http://cdn.domain.com/static/images/springsource.png"
+    }
+    
+}
